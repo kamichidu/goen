cef6d8b70126248ad6721f0dc2acfd7ebc5099c7
kamichidu <c.kamunagi@gmail.com>
2018-07-30 13:07:59 +0900

    change version rule

--- _tools/update-version.sh
+++ _tools/update-version.sh
@@ -5,8 +5,8 @@ set -e -u
 basedir="$(dirname "$(dirname "$0")")"
 verfile="$basedir/cmd/goen/version.go"
 
-version="$(git describe --tags --always --dirty )"
+version="$(git describe --tags --exact-match)"
 
 sed -i -e 's/version = "[^"]*"/version = "'$version'"/' "$verfile"
 
-git commit -i "$verfile" -m 'Update version'
+git commit -i "$verfile" -m 'update version'
