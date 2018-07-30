5b1e1046bcdd4d998f691b70df852c374e8d9f31
kamichidu <c.kamunagi@gmail.com>
2018-07-30 13:58:49 +0900

    provide new version

--- _tools/update-version.sh
+++ _tools/update-version.sh
@@ -2,11 +2,15 @@
 
 set -e -u
 
+version="$1"
+
 basedir="$(dirname "$(dirname "$0")")"
 verfile="$basedir/cmd/goen/version.go"
 
-version="$(git describe --tags --exact-match)"
-
 sed -i -e 's/version = "[^"]*"/version = "'$version'"/' "$verfile"
 
+echo "commit $verfile" 1>&2
 git commit -i "$verfile" -m 'update version'
+
+echo "tag $version" 1>&2
+git tag "$version"
